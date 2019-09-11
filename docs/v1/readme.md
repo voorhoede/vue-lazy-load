6776aaefa3a242a0aa0a221def2c17148142171f
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-09-11 16:05:49 +0200

    add lazy-load component

--- docs/v1/readme.md
+++ docs/v1/readme.md
@@ -2,6 +2,8 @@
 
 **Component to lazy load content using the Intersection Observer API**
 
+This compenent uses the [Intersection observer API](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API) to detect when its content enters or exists the browser's viewport. The content only loads just before it comes into view, reducing initial page weight and load time.
+
 [[toc]]
 
 ## Installation
@@ -35,7 +37,7 @@ Vue.use(VueLazyLoad)
 ## Examples
 
 ::: demo
-<vue-lazy-load></vue-lazy-load>
+<vue-lazy-load><b>This content is lazy loaded</b></vue-lazy-load>
 :::
 
 <!-- The API section is auto generated, don't touch please -->
@@ -43,3 +45,19 @@ Vue.use(VueLazyLoad)
 ## API
 
 ### vue-lazy-load 
+
+#### slots 
+
+- `default` 
+
+- `placeholder` Content that is loaded as a placeholder until it comes into view 
+
+#### props 
+
+- `root-margin` ***String*** (*optional*) `default: '0px 0px 0px 0px'` 
+
+  See InetersectionOberserver rootMargin [docs](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options) 
+
+- `threshold` ***Number|Array*** (*optional*) 
+
+  See InetersectionOberserver treshold [docs](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options) 
--- src/vue-lazy-load.vue
+++ src/vue-lazy-load.vue
@@ -1,10 +1,70 @@
-
 <template>
-  <div>@voorhoede/vue-lazy-load</div>
+  <div class="vue-lazy-load">
+    <slot v-if="isIntersected" />
+    <!-- Content that is loaded as a placeholder until it comes into view -->
+    <slot v-if="!isIntersected" name="placeholder" />
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'VueLazyLoad',
+  props: {
+    /**
+     * See InetersectionOberserver rootMargin [docs](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options)
+     */
+    rootMargin: {
+      type: String,
+      default: '0px 0px 0px 0px',
+    },
+    /**
+     * See InetersectionOberserver treshold [docs](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options)
+     */
+    threshold: {
+      type: [Number, Array],
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      isIntersected: false,
+      observer: null,
+    }
+  },
+  mounted() {
+    if ('IntersectionObserver' in window) {
+      this.observe()
+    } else {
+      this.isIntersected = true
+    }
+  },
+  beforeDestroy() {
+    this.unobserve()
+  },
+  methods: {
+    observe() {
+      const { rootMargin, threshold } = this
+      const config = { root: undefined, rootMargin, threshold }
+      this.observer = new IntersectionObserver(this.onIntersection, config)
+      this.observer.observe(this.$el)
+    },
+    onIntersection(entries) {
+      this.isIntersected = entries.some((entry) => entry.intersectionRatio > 0)
+      if (this.isIntersected) {
+        this.unobserve()
+      }
+    },
+    unobserve() {
+      if ('IntersectionObserver' in window) {
+        this.observer.unobserve(this.$el)
+      }
+    },
+  },
 }
 </script>
+
+<style scoped>
+.vue-lazy-load {
+  min-width: 1px;
+  min-height: 1px;
+}
+</style>
